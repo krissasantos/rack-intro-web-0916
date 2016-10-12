67fcec088366758c0505c69fa1a5eb93c1a09c73
krissasantos <krissa.santos31@gmail.com>
2016-10-12 11:23:03 -0400

    Done.

--- application.rb
+++ application.rb
@@ -1,8 +1,10 @@
+
+
 class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is Krissa!"
     resp.finish
   end
 
--- config.ru
+++ config.ru
@@ -1,3 +1,8 @@
 require_relative "./application.rb"
 
 run Application.new
+
+
+#config.ru
+# require_relative "./application.rb"
+# bundle exec rackup
