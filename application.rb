f06caa29e86502eb9d9301241d00618517a3643f
krissasantos <krissa.santos31@gmail.com>
2016-10-13 10:08:35 -0400

    Done.

--- application.rb
+++ application.rb
@@ -10,3 +10,23 @@ class Application
 
 end
 
+
+
+# def call(env)
+#   if env['REQUEST_PATH'] == '/' #there's a nicer way using the 'request' library on Rack.
+#    #
+#     ['200', {'Content-Type' => 'text/html'}, ['A barebones rack app.']]
+#   else
+#     ['404', {'Content-Type' => 'text/html'}, ['']]
+#   end
+# end
+
+
+
+# there's a nicer way using the 'request' library on Rack.
+
+# request = Rack::Request.new(env)
+# if request.path == '/'
+#   response = Rack::Response.new('<h1>Web 0916 Book App</h1>')
+#   #   instead of manually:  ['200', {'Content-Type' => 'text/html'}, ['A barebones rack app.']]
+
